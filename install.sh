ae91c3440c4210d306349060640b03798d2de8f7
Jose Membreno <jmembreno90217@gmail.com>
2020-10-31 19:49:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,10 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # Vundle Installation (Git required)
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Vim Plug Installation
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # Syntastic Syntax Checkers Installation
 # Pylint
 sudo apt-get install pylint
@@ -20,10 +24,6 @@ sudo npm install -g csslint
 # Javac
 sudo apt install default-jre
 sudo apt install default-jdk
-
-# Vim Plug Installation
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # YouCompleteMe Installation
 sudo apt install build-essential cmake vim python3-dev
