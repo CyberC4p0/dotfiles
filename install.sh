3ca4f49710f5d7c5b5f096250ce3322ebdba71a6
Jose Membreno <jmembreno90217@gmail.com>
2020-10-31 21:56:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# Vim Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # Syntastic Syntax Checkers Installation
 # Pylint
 sudo apt-get install pylint
@@ -17,8 +24,3 @@ sudo npm install -g csslint
 # Javac
 sudo apt install default-jre
 sudo apt install default-jdk
-
-# YouCompleteMe Installation
-sudo apt install build-essential cmake vim python3-dev
-cd ~/.vim/plugged/YouCompleteMe
-./install.py
