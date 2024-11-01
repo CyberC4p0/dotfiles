aa105aea7d8bac00687966f0aa2f6d91107958a0
cyberc4p0 <jmembreno90217@gmail.com>
2024-11-01 18:39:51 -0400

    Removed swap set up from script

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,17 +1,4 @@
-# If no swap, leave empty
-read -p "GB OF SWAP:" swap
-
-if [ -n "$swap"]; then
-
-	sudo dd if=/dev/zero of=/swapfile bs=1G count="$swap"
-	sudo chmod 600 /swapfile
-	sudo mkswap /swapfile
-	sudo swapon /swapfile
-	echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab 
-	echo -e '\n# Enable swap\nsudo swapon /swapfile' >> ~/.zshrc
-
-fi
-
+# Load ZSH Aliases
 echo -e '\n# Load aliases\nsource ~/.zsh_aliases' >> ~/.zshrc
 
 # Install Preffered Apps
